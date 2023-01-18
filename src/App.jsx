819339d21b647ff8708be0943e02415d8a51faa2
lockdur <matvej.novik@gmail.com>
2023-01-18 17:28:16 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,24 @@ import './scss/index.scss';
 import MakeOrder from './components/MakeOrder';
 
 function App() {
-  const supportsContainerQueries =
-    'container' in document.documentElement.style;
-  if (!supportsContainerQueries) {
-    import('container-query-polyfill');
-  }
+  import('container-query-polyfill');
+
   return (
     <div className="App">
       <MakeOrder jsonData={document.querySelector('#root').dataset.info} />
+      <div class="info">
+        <p>
+          ℹ️ Your browser does not support Container Queries{' '}
+          <em>(or only supports an older syntax)</em>. To plug this hole,{' '}
+          <a
+            href="https://www.npmjs.com/package/container-query-polyfill"
+            target="_top"
+          >
+            a polyfill
+          </a>{' '}
+          has been loaded.
+        </p>
+      </div>
     </div>
   );
 }
